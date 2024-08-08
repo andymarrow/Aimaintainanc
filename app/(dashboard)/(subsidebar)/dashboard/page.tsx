2f4b7bacccf0ec8797ede8baed71f2222b267ee5
Saron <118249123+Saron635@users.noreply.github.com>
2024-08-08 08:29:56 +0300

    Delete app directory

--- app/(dashboard)/(subsidebar)/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-const DashboardPage = () => {
-    return ( 
-        <div className="flex justify-center items-center text-center">
-            <h1> Dashboard Page</h1>
-        </div>
-     );
-}
- 
-export default DashboardPage;
